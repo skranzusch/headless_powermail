4b46cf87bcea7ff89ac427a5a6e2560c663432b5
Philipp Kitzberger <coding@kitze.net>
2023-05-11 18:41:48 +0200

    Add RedirectFinisher that performs redirect with JSON response
    Resolves: #25

---
+++ Classes/Finisher/RedirectFinisher.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+namespace FriendsOfTYPO3\HeadlessPowermail\Finisher;
+
+use In2code\Powermail\Domain\Service\RedirectUriService;
+use In2code\Powermail\Utility\FrontendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class RedirectFinisher extends \In2code\Powermail\Finisher\RedirectFinisher
+{
+    /**
+     * Redirect user after form submit
+     *
+     * @return void
+     */
+    public function redirectToUriFinisher(): void
+    {
+        $redirectService = GeneralUtility::makeInstance(RedirectUriService::class, $this->contentObject);
+        $uri = $redirectService->getRedirectUri();
+        if (!empty($uri) && $this->isRedirectEnabled()) {
+            echo json_encode([
+                'redirectUrl' => $uri,
+                'statusCode' => 303,
+            ]);
+            die;
+        }
+    }
+}
